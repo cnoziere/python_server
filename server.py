1443c66757716d7c8e09cc9aaf843afd451d60dd
cnoziere <cmn72@cornell.edu>
2015-07-19 13:50:14 -0700

    add html and update server

--- server.py
+++ server.py
@@ -6,16 +6,26 @@ PORT = 8000
 
 class SimpleHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
-        print self.command, self.headers
-        message = 'Hello {}.'.format(self.address_string())
-        self.send_response(1, message)
+        try:
+            f = open("chat.html")
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            #self.wfile.write(f.read())
+            f.close()
+            return
+        except IOError:
+            self.send_error(404, 'File Not Found')
 
-
-HOST, PORT = '10.211.200.242', 8000
-handler = SimpleHTTPHandler
-http_daemon = SocketServer.TCPServer((HOST, PORT), handler)
-
-print "serving at port", PORT
+def main():
+    try:
+        HOST, PORT = 'localhost', 8000
+        handler = SimpleHTTPHandler
+        server = BaseHTTPServer.HTTPServer((HOST, PORT), handler)
+        print "serving at port", PORT
+        server.serve_forever()
+    except:
+        pass
 
 if __name__ == "__main__":
-    http_daemon.serve_forever()
+    main()
