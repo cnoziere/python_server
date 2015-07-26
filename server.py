422349b4f4f4d71c71fe77c07eb508498dd2fcae
Leo Mehr <ljm262@cornell.edu>
2015-07-25 21:40:44 -0700

    Various restructuring
    1. Add website page structure
    2. More robust server file handling
    3. Style changes

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
-import sys
+''' This simple server handles client HTTP requests '''
+
+import sys, os
 import BaseHTTPServer
 import mimetypes
 
-PORT = 8000
+DEFAULT_PORT = 8000
+SITE_ROOT = os.getcwd()
 
 class SimpleHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    ''' implement do_X in order to specify the server's behavior when handling
+        an HTTP X request '''
+
     def do_GET(self):
-        print self.path
         f = self.load_file()
         if not f:
             return
+        file_type = mimetypes.guess_type(f.name, strict=True)[0]
+        if file_type == None:
+            file_type = 'text/plain'
+
         self.send_response(200)
-        ''' mimetypes.guess_type returns a tuple (type, encoding) where type
-        is None if the type can't be determined or a string of the form
-        type/subtype. '''
-        self.send_header('Content-type', mimetypes.guess_type(self.path, strict=True)[0])
+        self.send_header('Content-type', file_type)
         self.end_headers()
         self.wfile.write(f.read())
         f.close()
@@ -22,31 +28,30 @@ class SimpleHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def load_file(self):
         ''' Determines which file to send as a response based on the request. '''
         try:
-            if self.path == '/':
-                return open('chat.html')
-            else :
-                return open(self.path[1:])
-
+            if len(self.path) > 1 and self.path[0] == '/':
+                self.path = self.path[1:]
+            file_path = os.path.join(SITE_ROOT, self.path)
+            if os.path.isfile(file_path):
+                return open(file_path)
+            else:
+                return open('index.html')
         except IOError:
             self.send_error(404, 'File Not Found')
             return None
 
 def main():
     try:
-        if len(sys.argv) > 1:
-            try:
-                PORT = int(sys.argv[1])
-            except:
-                PORT = 8000
-        else:
-            PORT = 8000
+        try:
+            PORT = int(sys.argv[1])
+        except:
+            PORT = DEFAULT_PORT
         HOST = 'localhost'
         handler = SimpleHTTPHandler
         server = BaseHTTPServer.HTTPServer((HOST, PORT), handler)
-        print "serving at port", PORT
+        print 'serving at port', PORT
         server.serve_forever()
-    except:
-        pass
+    except Exception as e:
+        print 'server failed to start: %s' % e.value
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
